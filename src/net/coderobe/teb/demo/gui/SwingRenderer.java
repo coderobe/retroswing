0bf145b8f476967d45771cab8a979ae73067a993
Simon Wagner <simon@simon-wagner.eu>
2018-11-02 22:24:44 +0100

    SwingRenderer: hopefully fix indentations

--- src/net/coderobe/teb/demo/gui/SwingRenderer.java
+++ src/net/coderobe/teb/demo/gui/SwingRenderer.java
@@ -13,9 +13,9 @@ import net.coderobe.teb.demo.iface.Renderer;
 public class SwingRenderer implements Renderer {
 	private Framebuffer fb;
 	private BufferedImage fbData;
-    private int zoom;
+	private int zoom;
 
-    private JFrame window;
+	private JFrame window;
 	private JPanel viewPanel;
 
 	public SwingRenderer(Framebuffer fb) {
@@ -23,43 +23,43 @@ public class SwingRenderer implements Renderer {
 	}
 
 	public SwingRenderer(Framebuffer fb, int zoom) {
-	    if (fb == null) {
-	        throw new IllegalArgumentException("Framebuffer must not be null!");
-        }
+		if (fb == null) {
+			throw new IllegalArgumentException("Framebuffer must not be null!");
+		}
 
-        this.fb = fb;
-        this.zoom = zoom;
+		this.fb = fb;
+		this.zoom = zoom;
 
-        fbData = new BufferedImage(fb.getWidth() * zoom, fb.getHeight() * zoom,
-                BufferedImage.TYPE_INT_RGB);
+		fbData = new BufferedImage(fb.getWidth() * zoom, fb.getHeight() * zoom,
+				BufferedImage.TYPE_INT_RGB);
 
-        viewPanel = new JPanel(){
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(fbData, 0, 0, this);
-            }
-        };
+		viewPanel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(fbData, 0, 0, null);
+			}
+		};
 
-        viewPanel.setPreferredSize(new Dimension(fbData.getWidth(), fbData.getHeight()));
-        viewPanel.setSize(fbData.getWidth(), fbData.getHeight());
+		viewPanel.setPreferredSize(new Dimension(fbData.getWidth(), fbData.getHeight()));
+		viewPanel.setSize(fbData.getWidth(), fbData.getHeight());
 
-        window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.getContentPane().setLayout(new FlowLayout());
-        window.getContentPane().add(viewPanel);
-        window.pack();
-        window.setVisible(true);
+		window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setLayout(new FlowLayout());
+		window.getContentPane().add(viewPanel);
+		window.pack();
+		window.setVisible(true);
 	}
 
-    public int getZoom() {
-        return zoom;
-    }
+	public int getZoom() {
+		return zoom;
+	}
 
-    public synchronized void setZoom(int zoom) {
+	public synchronized void setZoom(int zoom) {
 		this.zoom = zoom;
 		fbData = new BufferedImage(fb.getWidth() * zoom, fb.getHeight() * zoom,
-                BufferedImage.TYPE_INT_RGB);
+				BufferedImage.TYPE_INT_RGB);
 	}
 
 	public JFrame getWindow() {
@@ -69,8 +69,8 @@ public class SwingRenderer implements Renderer {
 	public synchronized void draw() {
 		for (int x = 0; x < fb.getWidth() * zoom; x++) {
 			for (int y = 0; y < fb.getHeight() * zoom; y++) {
-                fbData.setRGB(x, y, fb.getRGB(x / zoom, y / zoom));
-            }
+				fbData.setRGB(x, y, fb.getRGB(x / zoom, y / zoom));
+			}
 		}
 
 		window.repaint();
