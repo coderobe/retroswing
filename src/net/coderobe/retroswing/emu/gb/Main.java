7f1b9dba5617468614fe02d61743825ffffd7931
Robin Broda <robin@broda.me>
2018-11-03 23:55:08 +0100

    emu.gb.Main: Read cartridge file, Start 'Core' loop

--- src/net/coderobe/retroswing/emu/gb/Main.java
+++ src/net/coderobe/retroswing/emu/gb/Main.java
@@ -1,8 +1,23 @@
 package net.coderobe.retroswing.emu.gb;
 
-public class Main {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		byte[] cartridge = Files.readAllBytes(Paths.get(args[0]));
+		
+		Core core = new Core();
+		
+		// Read cartridge into memory
+		int mem_cart = 0x0000;
+		for (byte b : cartridge) {
+			core.mmu.ram.put((short) mem_cart++, b);
+		}
+		
+		while(true) {
+			core.tick();
+		}
 	}
 }
