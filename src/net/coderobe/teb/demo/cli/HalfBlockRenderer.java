cfba4c40f3129227f0174b0e60a5b4a1410bff64
Robin Broda <robin@broda.me>
2018-11-03 19:40:02 +0100

    HalfBlockRenderer: Comments

--- src/net/coderobe/teb/demo/cli/HalfBlockRenderer.java
+++ src/net/coderobe/teb/demo/cli/HalfBlockRenderer.java
@@ -34,7 +34,7 @@ public class HalfBlockRenderer implements Renderer {
 			for(int x = 0; x < fb.getWidth(); x++) {
 				Color c_top = new Color(fb.getRGB(x, y));
 				Color c_bot = new Color(fb.getRGB(x, y+1));
-				if(!c_top.equals(c_top_prev)) {
+				if(!c_top.equals(c_top_prev)) { // don't set fg color when already on correct color
 					strb.append(
 						"\033[38;2;" + // ANSI 256-color foreground set RGB
 						c_top.getRed() + ";" +
@@ -43,10 +43,11 @@ public class HalfBlockRenderer implements Renderer {
 					);
 					c_top_prev = c_top;
 				}
-				if(!c_bot.equals(c_bot_prev)) {
+				if(!c_bot.equals(c_bot_prev)) { // don't set bg color when already on correct color
 					if(y+1 >= fb.getHeight()) {
+						// lower half of last row unused, reset color
 						strb.append("\033[49m");
-					}else {
+					} else {
 						strb.append(
 							"\033[48;2;" + // ANSI 256-color background set RGB
 							c_bot.getRed() + ";" +
@@ -56,18 +57,15 @@ public class HalfBlockRenderer implements Renderer {
 					}
 					c_bot_prev = c_bot;
 				}
-				strb.append("\u2580");
+				strb.append("\u2580"); // Unicode UPPER HALF BLOCK (▀)
 			}
-			strb.append("\033[1B\033["+fb.getWidth()+"D");
+			strb.append("\033[1B\033["+fb.getWidth()+"D"); // move down one line and to X=0
 		}
-		//strb.append("\033[1;0f" + fb.getWidth() + "x" + fb.getHeight()); // debug info
-		//strb.append("\033[" + fb.getHeight() + ";" + 0 + "f");
 		ps.print(strb);
 		ps.flush(); // necessary?
 	}
 	public void close() {
 		ps.print("\033[0m"); // reset graphics rendition
 		ps.print("\033[?25h"); // show cursor
-		ps.flush();
 	}
 }
