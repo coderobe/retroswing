823e187113abc8621af9d05027977497d35ee73b
Robin Broda <robin@broda.me>
2018-11-04 19:39:17 +0100

    emu.gb.Memory: Fix 16-bit load arithmetic

--- src/net/coderobe/retroswing/emu/gb/Memory.java
+++ src/net/coderobe/retroswing/emu/gb/Memory.java
@@ -138,9 +138,9 @@ public class Memory {
 				return PC;
 			} else {
 				short ret = 0x0000;
-				ret |= reg_8.get(((String) r).charAt(0));
+				ret |= Byte.toUnsignedInt(reg_8.get(((String) r).charAt(0)));
 				ret <<= 8;
-				ret |= reg_8.get(((String) r).charAt(1));
+				ret |= Byte.toUnsignedInt(reg_8.get(((String) r).charAt(1)));
 				return ret;
 			}
 		}
