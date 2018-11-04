cfe63ee0d51069c69533317c64a27fb794f9410f
Robin Broda <robin@broda.me>
2018-11-04 01:47:52 +0100

    emu.gb.Main: Add Power Up Sequence (source: Game Boy CPU Manual 1.01 (DP))

--- src/net/coderobe/retroswing/emu/gb/Main.java
+++ src/net/coderobe/retroswing/emu/gb/Main.java
@@ -10,6 +10,45 @@ public class Main {
 		
 		Core core = new Core();
 		
+		// Power Up Sequence
+		core.mmu.reg_8.put('A', (byte) 0x01); // GB
+		core.mmu.reg_8.put('F', (byte) 0xB0);
+		core.mmu.reg_16.put("BC", (short) 0x0013);
+		core.mmu.reg_16.put("DE", (short) 0x00D8);
+		core.mmu.reg_16.put("HL", (short) 0x014D);
+		core.mmu.SP = (short) 0xFFFE;
+		core.mmu.ram.put((short) 0xFF05, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF06, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF07, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF10, (byte) 0x80);
+		core.mmu.ram.put((short) 0xFF11, (byte) 0xBF);
+		core.mmu.ram.put((short) 0xFF12, (byte) 0xF3);
+		core.mmu.ram.put((short) 0xFF14, (byte) 0xBF);
+		core.mmu.ram.put((short) 0xFF16, (byte) 0x3F);
+		core.mmu.ram.put((short) 0xFF17, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF19, (byte) 0xBF);
+		core.mmu.ram.put((short) 0xFF1A, (byte) 0x7F);
+		core.mmu.ram.put((short) 0xFF1B, (byte) 0xFF);
+		core.mmu.ram.put((short) 0xFF1C, (byte) 0x9F);
+		core.mmu.ram.put((short) 0xFF1E, (byte) 0xBF);
+		core.mmu.ram.put((short) 0xFF20, (byte) 0xFF);
+		core.mmu.ram.put((short) 0xFF21, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF22, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF23, (byte) 0xBF);
+		core.mmu.ram.put((short) 0xFF24, (byte) 0x77);
+		core.mmu.ram.put((short) 0xFF25, (byte) 0xF3);
+		core.mmu.ram.put((short) 0xFF26, (byte) 0xF1); // GB
+		core.mmu.ram.put((short) 0xFF40, (byte) 0x91);
+		core.mmu.ram.put((short) 0xFF42, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF43, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF45, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF47, (byte) 0xFC);
+		core.mmu.ram.put((short) 0xFF48, (byte) 0xFF);
+		core.mmu.ram.put((short) 0xFF49, (byte) 0xFF);
+		core.mmu.ram.put((short) 0xFF4A, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFF4B, (byte) 0x00);
+		core.mmu.ram.put((short) 0xFFFF, (byte) 0x00);
+		
 		// Read cartridge into memory
 		int mem_cart = 0x0000;
 		for (byte b : cartridge) {
