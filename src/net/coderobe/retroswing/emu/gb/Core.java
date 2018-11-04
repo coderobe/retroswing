70744da370d2c8ef5c2737a2bdef66b05ea5af50
Robin Broda <robin@broda.me>
2018-11-04 02:50:24 +0100

    emu.gb.Core: Fix UnknownOpcodeException msg - explicit cast from promoted int to fix address in message

--- src/net/coderobe/retroswing/emu/gb/Core.java
+++ src/net/coderobe/retroswing/emu/gb/Core.java
@@ -9,7 +9,7 @@ public class Core {
 		try {
 			opcodes.get(mmu.ram.get(mmu.PC++)).exec();
 		} catch(NullPointerException e) {
-			throw new UnknownOpcodeException("Unknown opcode "+String.format("0x%02X", mmu.ram.get((short)(mmu.PC-1)))+" at address "+String.format("0x%04X", mmu.PC-1));
+			throw new UnknownOpcodeException("Unknown opcode "+String.format("0x%02X", mmu.ram.get((short)(mmu.PC-1)))+" at address "+String.format("0x%04X", (short)(mmu.PC-1)));
 		}
 	}
 	private final Map<Byte, Opcode> opcodes = new HashMap<Byte, Opcode>(){{
